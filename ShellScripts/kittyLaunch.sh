5d4c3c00c333f6a45ff9408c91f48a6bb6210955
Szymon Grucela <szymonmlbb@gmail.com>
2025-01-15 17:56:46 +0100

    missclick

--- ShellScripts/kittyLaunch.sh
+++ ShellScripts/kittyLaunch.sh
@@ -3,7 +3,7 @@ kitty -c ~/DotFiles/Kitty/kitty.conf zsh &
 
 WALLPAPER_PATH="$(find "$HOME/Wallpaper/Current" -type f | head -n 1)"
 
-sleep 0.5q
+sleep 0.5
 
 hellwal -i "$WALLPAPER_PATH"
 wal -i "$WALLPAPER_PATH"
