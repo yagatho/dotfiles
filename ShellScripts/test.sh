321caff4955bfdeea3e296a12be460a00ba661af
Szymon Grucela <szymonmlbb@gmail.com>
2025-03-24 00:27:11 +0100

    commit

--- ShellScripts/test.sh
+++ ShellScripts/test.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-killall dunst
-dunst &
-notify-send -u low "Reminder" "Don't forget to take a break!"
-notify-send -u critical "Reminder" "Don't forget to take a break!"
-notify-send -u normal "Reminder" "Don't forget to take a break!"
-notify-send -h int:value:42 "Working ..."
+#!/usr/bin/env bash
+
+some_var=echo
+text='*.sh'
+
+${some_var} ${text}
