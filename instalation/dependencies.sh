f0256ab69eac311b3d64d5e1b6f6bef3363701d8
yagathovm <szymonmlbb@gmail>
2025-10-19 16:24:19 +0000

    some minor bugfixes in the installation script

--- instalation/dependencies.sh
+++ instalation/dependencies.sh
@@ -4,7 +4,7 @@ sudo pacman -S wl-clipboard man tldr git ttf-nerd-fonts-symbols-mono python dotn
 
 
 # AUR
-read -p "Install and compile wezterm? (May take long time)(Needs paru)(Y/N): " confirm && [[ $confirm != [yY] ]] || paru -S wezterm-git
+read -p "Install wezterm? (May take long time)(Needs paru)(Y/N): " confirm && [[ $confirm != [yY] ]] || paru -S wezterm-git
 
 read -p "Install tidal? (Needs paru)(Y/N): " confirm && [[ $confirm != [yY] ]] || paru -S tidal-hifi-bin
 
@@ -14,11 +14,12 @@ read -p "Install unityhub? (Needs paru)(Y/N): " confirm && [[ $confirm != [yY] ]
 # Other misc
 # Enable multilib
 sudo mv ~/git/dotfiles/misc/pacman.conf /etc/pacman.conf
+sudo pacman -Syu
 
 read -p "Install blender? (Y/N): " confirm && [[ $confirm != [yY] ]] || sudo pacman -S blender
 
 read -p "Install krita? (Y/N): " confirm && [[ $confirm != [yY] ]] || sudo pacman -S krita
 
-read -p "Install vm?(Y/N): " confirm && [[ $confirm != [yY] ]] || ~/git/dotfiles/instalation/vm.sh
+read -p "Install vm? (Y/N): " confirm && [[ $confirm != [yY] ]] || ~/git/dotfiles/instalation/vm.sh
 
-read -p "Install steam?(Y/N): " confirm && [[ $confirm != [yY] ]] || sudo pacman -S steam
+read -p "Install steam? (Y/N): " confirm && [[ $confirm != [yY] ]] || sudo pacman -S steam
